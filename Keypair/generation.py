7e7585ae8cfc62f5274ff9d8b90a273693881f7b
Hieu T. Nguyen <nguyentronghieu@hsgs.edu.vn>
2023-10-03 20:02:25 +0700

    add hash func

--- Keypair/generation.py
+++ Keypair/generation.py
@@ -2,8 +2,8 @@ from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric import ec
 
 def generate_key_pair():
-    private_key = ec.generate_private_key(ec.SECP256R1())
-    public_key = private_key.public_key()
+    private_key = ec.generate_private_key(ec.SECP256R1())  # Tạo private key ECC
+    public_key = private_key.public_key()  # Tạo public key từ private key
 
     private_key_hex = private_key.private_numbers().private_value
     public_key_hex = public_key.public_numbers().y
