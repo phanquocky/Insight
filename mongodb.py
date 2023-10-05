93431731d5df792b4cc64a227aab11d0c7cef4bd
quangphat18ti <70308391+quangphat18ti@users.noreply.github.com>
2023-10-05 15:45:42 +0700

    query_users_by_score

--- mongodb.py
+++ mongodb.py
@@ -53,3 +53,53 @@ def query_users_by_name(name):
     
     print(listUser)
     return listUser
+
+def query_all_users():
+    # Truy vấn cơ sở dữ liệu để lấy danh sách tất cả người dùng
+    users_collection = db['User']
+    users = users_collection.find({})
+
+    listUser = []
+
+    # In ra màn hình danh sách 
+    for user in users:
+        userProfile = {
+            "id": user['_id'],
+            "name": user['name'],
+            "public_key": user['public_key'],
+            "score": user['score']
+        }
+        listUser.append(userProfile)
+    
+    print("all users: ")
+    print("number of users: ", len(listUser))
+    print(listUser)
+    return listUser
+
+def query_users_by_score(min_score = 0, max_score = 100, num_users = 20): 
+    # Truy vấn cơ sở dữ liệu để lấy danh sách người dùng có điểm số trong khoảng $min_score và $max_score
+    users_collection = db['User']
+    users = users_collection.find({'score': {'$gt': min_score, '$lt': max_score}}).limit(num_users)
+
+    listUser = []
+
+    # In ra màn hình danh sách 
+    for user in users:
+        userProfile = {
+            "id": user['_id'],
+            "name": user['name'],
+            "public_key": user['public_key'],
+            "score": user['score']
+        }
+        listUser.append(userProfile)
+    
+    print("users with score in range: ", min_score, " - ", max_score)  
+    print("number of users: ", len(listUser))
+    print(listUser)
+    return listUser
+
+def update_user_score(id, score):
+    # Cập nhật điểm số của người dùng có id là $id
+    users_collection = db['User']
+    users_collection.update_one({'_id': id}, {'$set': {'score': score}})
+    print("Update user score successfully!")
