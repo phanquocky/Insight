95d6ea8c253816881ce0be80684214ad1c5cc011
phanquocky <20120017@student.hcmus.edu.vn>
2023-10-05 14:00:47 +0700

    Add bootstrap5 and navbar

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return render_template("base.html")
 
 @app.route("/search", methods=['GET', 'POST'])
 def search():
